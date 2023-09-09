f5343b38527a3fbf2e16be82d118b85c01d5d750
qiang <2844326483@qq,com>
2023-09-09 11:11:57 +0800

    1# Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.## On branch master# Changes to be committed:#	modified:   animal.py#

--- animal.py
+++ animal.py
@@ -3,22 +3,17 @@ import sys
 def default():
     print('hello')
 
-<<<<<<< HEAD
 def cat():
     print('Meow')
 
-
-def main():
-    if sys.argv[1] == 'cat':
-        cat()
-=======
 def dog():
     print('Woof!')
 
 def main():
     if sys.argv[1] == 'dog':
         dog()
->>>>>>> dog
+    elif sys.argv[1] == 'cat':
+        cat()
     else:
         default()
 
